832dd9f22d28591d2b03c8f2993423fdc6d3446c
SYED <dadapeer.s@prodapt.com>
2024-02-19 17:02:52 +0530

    removing secret keys

--- first_ec2.tf
+++ first_ec2.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "<aws-access-key"
-  secret_key = "aws-secret-key"
+  access_key = "<aws-access-key>"
+  secret_key = "<aws-secret-key>"
 }
 
 resource "aws_instance" "myec2" {
